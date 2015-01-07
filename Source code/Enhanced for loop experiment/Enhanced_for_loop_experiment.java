7e0abe1fd3b8400a18e3a738885157426f76c2b5
Ｖ字龍(Vdragon) <Vdragon.Taiwan@gmail.com>
2015-01-07 17:51:55 +0800

    新增「增強型 for 迴圈實驗程式」
    Signed-off-by: Ｖ字龍(Vdragon) <Vdragon.Taiwan@gmail.com>

---
+++ Source code/Enhanced for loop experiment/Enhanced_for_loop_experiment.java
@@ -0,0 +1,33 @@
+/**
+ * @file Enhanced_for_loop_experiment.java
+ * @brief 增強型 for 迴圈實驗程式
+ * @author Vdragon
+ */
+import static java.lang.System.*;
+
+/**
+ * @brief 放置 Java 程式進入點的類別
+ */
+public class Enhanced_for_loop_experiment {
+	private Enhanced_for_loop_experiment() {
+		/* 不允許實例化 */
+	}
+
+	/**
+	 * @brief Java 程式進入點
+	 * @param args 包含命令列參數的 String 陣列
+	 */
+	public static void main(String[] args) {
+		int numbers[] = {12, 34, 56, 78, 90};
+		String fruit_names[] = {"apple", "banana", "pear"};
+		
+		for (int number : numbers){
+			out.println(number);
+		}
+		
+		for (String fruit_name : fruit_names){
+			out.println(fruit_name);
+		}
+	}
+
+}
